561929cdbe1dc9438d0656198a38796ce5972166
putilove <gleb89047408502@gmail.com>
2022-12-23 16:34:35 +0400

    something files

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import {BrowserRouter} from "react-router-dom";
+import AppRouter from "./components/AppRouter";
+import NavBar from "./components/NavBar";
+import {observer} from "mobx-react-lite";
+import {useContext, useEffect, useState} from "react";
+import {Context} from "./index";
+import {checkAuth} from "./http/userAPI";
+import {Spinner} from "react-bootstrap";
+
+const App = observer(() => {
+    const {user} = useContext(Context)
+    const {posts} = useContext(Context)
+    console.log(posts)
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        checkAuth().then(data => {
+            user.setUser(data)
+            user.setIsAuth(true)
+        }).finally(() => setLoading(false))
+    }, [])
+
+    if (loading) return <Spinner animation={"grow"}/>
+
+    return (
+        <BrowserRouter>
+            <NavBar />
+           <AppRouter />
+        </BrowserRouter>
+    );
+});
+
+export default App;
